c768f62ad87039920bb7759275c51ee6e3de131f
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2025-04-27 08:47:09 +0300

    I want hostinggg

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import About from "./About";
 import Serdica from "./Serdica";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
+      {}
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/serdica" element={<Serdica />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
